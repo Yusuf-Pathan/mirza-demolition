a9cda4e7cfb4ab86587e64df322ff1ebd972f923
Yusuf Pathan <mirza4387@gmail.com>
2023-04-12 12:22:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { CategoryComponent } from './components/category/category.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LandingComponent],
+  declarations: [AppComponent, HeaderComponent, LandingComponent, CategoryComponent, ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +25,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
